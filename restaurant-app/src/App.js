c827e1437f92bb3b9c6d3875e55419101ce7507e
Nitesh Kumar <niteshkumar240198@gmail.com>
2023-09-25 12:15:23 +0530

    routes added

---
+++ restaurant-app/src/App.js
@@ -0,0 +1,14 @@
+
+import AllRouters from './components/AllRouters';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRouters />
+    </div>
+  );
+}
+
+export default App;
